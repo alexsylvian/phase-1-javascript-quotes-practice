962a7e9eea726f1d3c02ec584def4945863ba9cf
alexsylvian <lazeratus1@gmail.com>
2023-08-22 21:24:48 -0400

    added quote list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const quoteList = document.getElementById('quote-list')
-    quoteList.addEventListener('click', () => {
-        console.log('init')
-    })
+
+    function renderList(){
+        fetch('http://localhost:3000/quotes?_embed=likes')
+        .then(res => res.json())
+        .then(quotes => {
+            quotes.forEach(quoteObject => {
+                const quoteLine = document.createElement('li')
+                quoteLine.innerHTML = `
+                <li class='quote-card'>
+                <blockquote class="blockquote">
+                  <p class="mb-0">${quoteObject.quote}</p>
+                  <footer class="blockquote-footer">${quoteObject.author}</footer>
+                  <br>
+                  <button class='btn-success'>Likes: <span>0</span></button>
+                  <button class='btn-danger'>Delete</button>
+                </blockquote>
+              </li>
+                `
+                quoteList.appendChild(quoteLine)
+            })
+        })
+    }
+
+    renderList()
 })

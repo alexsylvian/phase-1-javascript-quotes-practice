5f937d55365a385c7b3b0840ab8127dfbd0cc912
alexsylvian <lazeratus1@gmail.com>
2023-08-22 21:12:16 -0400

    initial commit + assigned quotelist

---
+++ src/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const quoteList = document.getElementById('quote-list')
+    quoteList.addEventListener('click', () => {
+        console.log('init')
+    })
+})

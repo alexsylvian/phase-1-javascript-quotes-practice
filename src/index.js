7ac04f1065575b210df16f619cd0d9824d032955
alexsylvian <lazeratus1@gmail.com>
2023-08-22 21:42:18 -0400

    added submit function, does not alter json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const quoteList = document.getElementById('quote-list')
+    const newQuoteForm = document.getElementById('new-quote-form')
 
     function renderList(){
         fetch('http://localhost:3000/quotes?_embed=likes')
@@ -21,6 +22,25 @@ document.addEventListener('DOMContentLoaded', () => {
                 quoteList.appendChild(quoteLine)
             })
         })
+    }
+
+    newQuoteForm.addEventListener('submit', addNewQuote)
+
+    function addNewQuote(e){
+        e.preventDefault()
+        const newQuoteLine = document.createElement('li')
+        newQuoteLine.innerHTML = `
+        <li class='quote-card'>
+        <blockquote class="blockquote">
+          <p class="mb-0">${e.target.quote.value}</p>
+          <footer class="blockquote-footer">${e.target.author.value}</footer>
+          <br>
+          <button class='btn-success'>Likes: <span>0</span></button>
+          <button class='btn-danger'>Delete</button>
+        </blockquote>
+      </li>
+        `
+        quoteList.appendChild(newQuoteLine)
     }
 
     renderList()

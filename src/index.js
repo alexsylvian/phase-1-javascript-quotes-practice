96374d77d8ff5272428e12adfb44f6b06a6b3d27
alexsylvian <lazeratus1@gmail.com>
2023-08-27 14:40:12 -0400

    final like

--- src/index.js
+++ src/index.js
@@ -1,46 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const quoteList = document.getElementById('quote-list')
-    const newQuoteForm = document.getElementById('new-quote-form')
+    const quoteList = document.getElementById('quote-list');
+    const newQuoteForm = document.getElementById('new-quote-form');
 
-    function renderList(){
-        quoteList.innerText = ''
+    function renderList() {
+        quoteList.innerText = '';
         fetch('http://localhost:3000/quotes?_embed=likes')
-        .then(res => res.json())
-        .then(quotes => {
-            quotes.forEach(quoteObject => {
-                const quoteLine = document.createElement('li')
-                quoteLine.innerHTML = `
-                <li class='quote-card'>
-                <blockquote class="blockquote">
-                  <p class="mb-0">${quoteObject.quote}</p>
-                  <footer class="blockquote-footer">${quoteObject.author}</footer>
-                  <br>
-                  <button class='btn-success'>Likes: <span>0</span></button>
-                  <button class='btn-danger'>Delete</button>
-                </blockquote>
-              </li>
-                `
-                quoteList.appendChild(quoteLine)
+            .then(res => res.json())
+            .then(quotes => {
+                quotes.forEach(quoteObject => {
+                    let likeCounter = quoteObject.likes.length;
 
-                const deleteButton = quoteLine.querySelector('.btn-danger')
-                deleteButton.addEventListener('click', (e) => {
-                    // e.target.parentElement.remove()
-                    fetch(`http://localhost:3000/quotes/${quoteObject.id}`, {
-                        method: 'DELETE',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                    })
-                    .then(() => renderList())
-                })
+                    const quoteLine = document.createElement('li');
+                    quoteLine.innerHTML = `
+                        <li class='quote-card'>
+                        <blockquote class="blockquote">
+                          <p class="mb-0">${quoteObject.quote}</p>
+                          <footer class="blockquote-footer">${quoteObject.author}</footer>
+                          <br>
+                          <button class='btn-success'>Likes: <span>${likeCounter}</span></button>
+                          <button class='btn-danger'>Delete</button>
+                        </blockquote>
+                      </li>
+                    `;
+                    quoteList.appendChild(quoteLine);
 
-                const likesButton = quoteLine.querySelector('.btn-success')
-                likesButton.addEventListener('click', () => {
-                    console.log('like')
-                })
-            })
-        })
+                    const deleteButton = quoteLine.querySelector('.btn-danger');
+                    deleteButton.addEventListener('click', (e) => {
+                        fetch(`http://localhost:3000/quotes/${quoteObject.id}`, {
+                            method: 'DELETE',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                        })
+                        .then(() => renderList());
+                    });
+
+                    const likesButton = quoteLine.querySelector('.btn-success');
+                    likesButton.addEventListener('click', () => {
+                        fetch('http://localhost:3000/likes', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                quoteId: quoteObject.id
+                            })
+                        })
+                        .then(res => res.json())
+                        .then(newLike => {
+                            const updatedLikeCounter = likeCounter + 1;
+                            likesButton.querySelector('span').textContent = updatedLikeCounter;
+                            likeCounter = updatedLikeCounter;  // Update the likeCounter for the current quote
+                        });
+                    });
+                });
+            });
     }
+
 
     newQuoteForm.addEventListener('submit', createNewQuote)
 
